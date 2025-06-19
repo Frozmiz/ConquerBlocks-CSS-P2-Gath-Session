1ce16354413d7ab46305e45b2276712ba03848ad
Alejandro Gonzalez <frozmiz@live.com>
2025-06-19 17:57:15 +0200

    feat(styles): improve responsive layout while preserving original design

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/",
+    base: '/ConquerBlocks-CSS-P2-Gath-Session/',
 });
 
 // Usa rutas relativas en los enlaces generados en el HTML
